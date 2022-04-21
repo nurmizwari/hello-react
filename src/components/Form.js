6197696bed20ac924d6df7bf96826414df1e41f0
nur mizwari <nurmizwari04@gmail.com>
2022-04-21 08:59:00 +0700

    menambahkan form tapi belom jalan

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { valu: "", kamu: "" };
+
+    this.handlechange = this.handlechange.bind(this);
+    this.handlesubmit = this.handlesubmit.bind(this);
+  }
+
+  handlechange(e) {
+    this.setState((state, props) => {
+      return { valu: e.target.value };
+    });
+  }
+
+  handlesubmit() {
+    alert("pesan input adalah = " + this.state.valu);
+  }
   render() {
     return (
-      <div>
-        <form>
-          <input type="submit" value="submit data" />
-        </form>
-      </div>
+      <form onSubmit={this.handlesubmit}>
+        <input type="text" value={this.state.valu} />
+        <input type="submit" value="submit data" />
+      </form>
     );
   }
 }
